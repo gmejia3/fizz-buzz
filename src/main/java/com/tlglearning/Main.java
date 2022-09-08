6b6a01082108a7385854bb8b18be0a7b696cf733
gmejia3 <gemejiajr@gmail.com>
2022-09-08 11:02:37 -0700

    Transitioned to Enum.

--- src/main/java/com/tlglearning/Main.java
+++ src/main/java/com/tlglearning/Main.java
@@ -1,15 +1,14 @@
 package com.tlglearning;
 
+import java.util.Set;
+
 public class Main {
 
   public static void main(String[] args) {
     int i = 1;
     do {
-      String value = FizzBuzz.valueOf(i);
-      System.out.println(value);
-      if (value.equals("fizzbuzz")) {
-        break;
-      }
+      Set<FizzBuzz> value = FizzBuzz.valueOf(i);
+      System.out.printf("%d: %s%n", i, value);
       i++;
     } while (i <= 100);
   }
